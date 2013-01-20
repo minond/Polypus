edfdd1bf7d9e5be7cbeea335b618b01816c4dda8
Marcos Minond <minond@beeblebrox.(none)>
2013-01-20 00:22:40 -0700

    updating template library

--- src/template.js
+++ src/template.js
@@ -48,7 +48,7 @@
 
 	/**
 	 * clean up a merge field. removes merge field type character and curly
-	  * brackets
+	 * brackets
 	 * @param string field
 	 * @param int padleft
 	 * @param int padright
@@ -170,77 +170,205 @@ var test = new Template(
 
 
 
+(function(global) {
+	"use strict";
 
+	var Template, get_merge_field_label, get_fieldless_string, has_merge_fields, 
+		parse_merge_fields, render_merge_fields;
 
-/**
- * returns a strings label
- * i.e. #{label #{mergefield}} => label, mergefield
- * @param string str
- * @return string
- */
-var get_merge_field_label = function(str) {
-	return str.substr(0, Math.min.apply(Math,
-		[ "\r", "\n", " " ].map(function(ch) {
-			return str.indexOf(ch);
-		}).filter(function(i) {
-			return i >= 0;
-		})
-	));
-};
+	/**
+	 * returns a strings label. i.e. #{label #{mergefield}} => label, mergefield
+	 * @param string str
+	 * @return string
+	 */
+	get_merge_field_label = function(str) {
+		return str.substr(0, Math.min.apply(Math,
+			[ "\r", "\n", " " ].map(function(ch) {
+				return str.indexOf(ch);
+			}).filter(function(i) {
+				return i >= 0;
+			})
+		));
+	};
 
-/**
- * parsed merge fields in a template string
- * @param string str
- * @return array
- */
-var parse_merge_fields = function(str, open, close) {
-	var strlen, i, ch, sub, subm, field, bracket = null,
-		pos = 0, parts = [""];
+	/**
+	 * simple merge field check. just checks if the open and close merge field
+	 * characters/delimeters are found in a string.
+	 * @param string str
+	 * @param string open
+	 * @param string close
+	 * @return boolean
+	 */
+	has_merge_fields = function(str, open, close) {
+		return !!str.match(open) && !!str.match(close);
+	};
 
-	for (strlen = str.length, i = 0; i < strlen; i++) {
-		ch = str[i];
+	/**
+	 * TODO: this is removing any whitespace after the merge field, but should be
+	 * limited to just space characters.
+	 * @param string field
+	 * @param string str
+	 * @return string
+	 */
+	get_fieldless_string = function(field, str) {
+		return str.replace(RegExp(field + "\\s{0,}"), "");
+	};
 
-		// found merge start
-		if (ch === open) {
-			// we're starting a new match group
-			if (bracket === null) {
-				bracket = 1;
-				pos = i + 1;
-			} else {
-				// we're still within the outter match group
-				bracket++;
+	/**
+	 * parses merge fields in a template string. returns an array open string
+	 * pieces that can be later use to merge in merge values.
+	 * @param string str
+	 * @return array
+	 */
+	parse_merge_fields = function(str, open, close, esc) {
+		var strlen, i, ch, next, sub, subm, field, bracket = null,
+			pos = 0, parts = [""];
+
+		for (strlen = str.length, i = 0; i < strlen; i++) {
+			ch = str[i];
+			next = str[i + 1];
+
+			if (ch === esc && (next === open || next === close)) {
+				// escaped delimeter, save and skip next time
+				parts[ parts.length - 1 ] += next;
+				i++;
+				continue;
+			} else if (ch === open) {
+				// found merge start, we're starting a new match group
+				if (bracket === null) {
+					bracket = 1;
+					pos = i + 1;
+				} else {
+					// we're still within the outter match group
+					bracket++;
+				}
+			} else if (ch === close) {
+				// merge end, or group end
+				bracket--;
+			} else if (bracket === null) {
+				// outside of merge field
+				parts[ parts.length - 1 ] += ch;
 			}
-		} else if (ch === close) {
-			// merge end, or group end
-			bracket--;
-		} else if (bracket === null) {
-			// outside of merge field
-			parts[ parts.length - 1 ] += ch;
+
+			// done?
+			if (bracket === 0) {
+				// reset, save current group, and start new match group
+				bracket = null;
+				sub = parse_merge_fields(str.substring(pos, i), open, close, esc);
+				sub.field = get_merge_field_label(sub.raw);
+				sub.compiled[0] = get_fieldless_string(
+					sub.field,
+					sub.compiled[0]
+				);
+
+				parts.push(sub, "");
+			}
 		}
 
-		// done?
-		if (bracket === 0) {
-			// reset, save current group, and start new match group
-			bracket = null;
-			sub = parse_merge_fields(str.substring(pos, i), open, close);
-			sub.field = get_merge_field_label(sub.raw);
-			parts.push(sub, "");
-			// parts.push(parse_merge_fields(
-				// str.substring(pos, i),
-				// open, close
-			// ), "");
+		return { raw: str, compiled: parts };
+	};
+
+	/**
+	 * @param array cstr
+	 * @param object fields
+	 * @return string
+	 */
+	render_merge_fields = function(cstr, fields) {
+		var str = [], i, j, len, inner_len, cur, inner_cur;
+
+		for (i = 0, len = cstr.compiled.length; i < len; i++) {
+			cur = cstr.compiled[ i ];
+
+			if (typeof cur === "string") {
+				// regular string
+				str.push(cur);
+			} else if (cur instanceof Object) {
+				if (cur.raw === cur.field) {
+					// single merge field
+					str.push(fields[ cur.field ]);
+				} else {
+					// list merge field
+					if (cur.field in fields && fields[ cur.field ] instanceof Array) {
+						inner_cur = fields[ cur.field ];
+
+						for (j = 0, inner_len = inner_cur.length; j < inner_len; j++) {
+							str.push(render_merge_fields(cur, inner_cur[ j ]));
+						}
+					}
+				}
+			}
 		}
-	}
 
-	return { raw: str, compiled: parts };
-};
+		return str.join("");
+	};
+
+	Template = global.Template2 = function CompiledTemplate(str, fields) {
+		var contents = parse_merge_fields(
+			str,
+			Template.config.open,
+			Template.config.close,
+			Template.config.esc
+		);
+
+		if (this instanceof Template) {
+			this.contents = contents;
+		} else {
+			return render_merge_fields(contents, fields);
+		}
+	};
+
+	Template.prototype.render = function(fields) {
+		return render_merge_fields(this.contents, fields);
+	};
+
+	Template.config = {
+		open: "{",
+		close: "}",
+		esc: "\\"
+	};
+
+	Template.api = {
+		get_merge_field_label: get_merge_field_label,
+		get_fieldless_string: get_fieldless_string,
+		has_merge_fields: has_merge_fields,
+		parse_merge_fields: parse_merge_fields,
+		render_merge_fields: render_merge_fields
+	};
+})(this);
+
+var message = new Template2(
+	"{users hi, \\{my\\} name is {name}\n" +
+	"i'm {age} years old\n" +
+	"and these are my favorite colors:\n" +
+	"{colors {color}, }" +
+	"-------------------------------\n\n}"
+);
+
+var str = message.render({
+	users: [
+		{ name: "Marcos", age: 23, colors: [ { color: "red" }, { color: "blue" } ] },
+		{ name: "Marcos", age: 23, colors: [ { color: "red" }, { color: "blue" } ] },
+		{ name: "Marcos", age: 23, colors: [ { color: "red" }, { color: "blue" } ] }
+	]
+});
+
+console.log(str);
+
+/*
+// var parts = parse_merge_fields("this my {users " + "<div>{first_name}</div><div>{last_name}</div>\n} string and merge: {merge}", "{", "}");
+// var parts = parse_merge_fields("this my field: {merge}", "{", "}");
+
+// var aa = render_merge_fields(parts, {
+	// merge: "hi",
+	// users: [
+		// { first_name: "Marcos", last_name: "Minond" },
+		// { first_name: "Andres", last_name: "Minond" }
+	// ]
+// });
 
 
-
-
-
-
-
+// console.log("DONE, %o made this: %s", parts, aa);
+*/
 
 
 
